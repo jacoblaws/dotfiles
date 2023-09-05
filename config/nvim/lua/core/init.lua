eac558a99bf91d34126e66b61757ab9452402e77
Jacob Laws <git@jacoblaws.dev>
2023-09-04 20:24:23 -0700

    neovim: bootstrap lazy.nvim plugin manager

---
+++ config/nvim/lua/core/init.lua
@@ -0,0 +1,25 @@
+-- bootstrap lazy.nvim plugin manager
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable',
+    lazypath
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- lazy.nvim configuration
+require('lazy').setup('plugins', {
+  defaults = {
+    lazy = true,
+  },
+
+  checker = {
+    notify = false,
+    enabled = false,
+  },
+})
