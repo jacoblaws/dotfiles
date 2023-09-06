65ea8b94ab0a8b8daa726afece582d34d0a61446
Jacob Laws <git@jacoblaws.dev>
2023-09-05 22:17:49 -0700

    neovim: add universal icons in core utils

---
+++ config/nvim/lua/core/utils/icons.lua
@@ -0,0 +1,23 @@
+return {
+  -- file
+  file_default = '󰈔',
+  file_modified = '',
+
+  -- folder
+  folder_open = '󰝰',
+  folder_closed = '󰉋',
+  folder_empty = '󰉒',
+
+  -- git
+  git = '󰊢',
+  git_add = '',
+  git_delete = '',
+  git_change = '',
+  git_staged = '✓',
+  git_unstaged = '✗',
+  git_renamed = '➜',
+  git_ignored = '◌',
+  git_conflict = '',
+  git_untracked = '★',
+  git_sign = '▎',
+}
