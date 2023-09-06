ad1a6319dcacbfc59b28a2760be9ea5d53f7a7fc
Jacob Laws <git@jacoblaws.dev>
2023-09-05 22:20:36 -0700

    neovim: add file, folder, and git icons to neo-tree

--- config/nvim/lua/plugins/editor/neo-tree.lua
+++ config/nvim/lua/plugins/editor/neo-tree.lua
@@ -2,11 +2,39 @@
 --
 -- for browsing the file system or other tree-like structures
 
+local icon = require('core.utils.icons')
+
 local opts = {
   hide_root_node = true,
   close_if_last_window = false,
   retain_hidden_root_ident = true,
   auto_clean_after_session_restore = true,
+
+  default_component_configs = {
+    indent = { padding = 0, indent_size = 1 },
+    modified = { symbol = icon.file_modified },
+
+    icon = {
+      default = icon.file_default,
+      folder_open = icon.folder_open,
+      folder_closed = icon.folder_closed,
+      folder_empty = icon.folder_empty,
+    },
+
+    git_status = {
+      symbols = {
+        added = icon.git_add,
+        deleted = icon.git_delete,
+        modified = icon.git_modified,
+        renamed = icon.git_renamed,
+        untracked = icon.git_untracked,
+        ignored = icon.git_ignored,
+        staged = icon.git_staged,
+        unstaged = icon.git_unstaged,
+        conflict = icon.git_conflict,
+      },
+    },
+  },
 }
 
 local config = function(_, opts)
