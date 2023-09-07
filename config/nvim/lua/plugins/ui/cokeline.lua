f6b6f4e2f8571a10cc194babb16464693d25ab9e
Jacob Laws <git@jacoblaws.dev>
2023-09-06 17:24:55 -0700

    neovim: add custom components to cokeline

--- config/nvim/lua/plugins/ui/cokeline.lua
+++ config/nvim/lua/plugins/ui/cokeline.lua
@@ -20,6 +20,47 @@ local opts = function()
         },
       },
     },
+
+    components = {
+      {
+        text = function(buffer)
+          local get_state = require('neo-tree.sources.manager').get_state
+          local renderer = require('neo-tree.ui.renderer')
+
+          local file_open = renderer.window_exists(get_state('filesystem'))
+          local buf_open = renderer.window_exists(get_state('buffers'))
+          local git_open = renderer.window_exists(get_state('git_status'))
+
+          local neotree_open = file_open or buf_open or git_open
+
+          if buffer.index == 1 and neotree_open then return ' ' end
+          return '' 
+        end,
+      },
+      {
+        text = function(buffer) return (buffer.index ~= 1) and icon.win_separator or ' ' end,
+      },
+      {
+        text = function(buffer) return ' ' .. buffer.devicon.icon end,
+      },
+      {
+        text = function(buffer) return buffer.unique_prefix end,
+      },
+      {
+        text = function(buffer) return buffer.filename .. ' ' end,
+      },
+      {
+        text = function(buffer)
+          if buffer.is_readonly then return icon.file_readonly end
+          if buffer.is_modified then return icon.file_modified end
+          return icon.buffer_close
+        end,
+        delete_buffer_on_left_click = true,
+      },
+      {
+        text = '  ',
+      },
+    },
   }
 end
 
