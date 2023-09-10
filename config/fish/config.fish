7e6e732eb562f4e8e3ee4dd98c70e136e83c43f2
Jacob Laws <git@jacoblaws.dev>
2023-09-10 02:13:36 -0700

    fish: add tide prompt config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,19 @@
 # general
 set -U fish_greeting
 
+# tide prompt
+set -g tide_left_prompt_items pwd git jobs status newline character
+set -g tide_right_prompt_items time os
+
+# tide icons
+set -g tide_git_icon ''
+set -g tide_jobs_icon ''
+set -g tide_os_icon ' '
+set -g tide_status_icon_failure ''
+set -g tide_character_icon ' ❯'
+set -g tide_pwd_icon '  '
+set -g tide_pwd_icon_home '  '
+set -g tide_time_format ' %H:%M:%S'
+
 # aliases
 alias lg='lazygit'
