0623cd96585caa50c654082c158f81c0f01d1292
Jacob Laws <git@jacoblaws.dev>
2023-09-15 17:52:35 -0700

    neovim: set font to bold for some elements in lualine everforest theme

--- config/nvim/lua/plugins/themes/lualine/everforest.lua
+++ config/nvim/lua/plugins/themes/lualine/everforest.lua
@@ -26,45 +26,45 @@ local palette = {
 
 local theme = {
   normal = {
-    a = { fg = palette.bg_dim, bg = palette.green },
-    b = { fg = palette.fg_dim,    bg = palette.bg_dim },
-    c = { fg = palette.fg_dim,    bg = palette.bg_dim },
+    a = { fg = palette.bg_dim, bg = palette.green, gui = 'bold' },
+    b = { fg = palette.fg_dim, bg = palette.bg_dim },
+    c = { fg = palette.fg_dim, bg = palette.bg_dim },
   },
 
   insert = {
-    a = { fg = palette.bg_dim, bg = palette.yellow },
-    b = { fg = palette.fg_dim,    bg = palette.bg_dim },
-    c = { fg = palette.fg_dim,    bg = palette.bg_dim },
+    a = { fg = palette.bg_dim, bg = palette.yellow, gui = 'bold' },
+    b = { fg = palette.fg_dim, bg = palette.bg_dim },
+    c = { fg = palette.fg_dim, bg = palette.bg_dim },
   },
 
   visual = {
-    a = { fg = palette.bg_dim, bg = palette.red },
-    b = { fg = palette.fg_dim,    bg = palette.bg_dim },
-    c = { fg = palette.fg_dim,    bg = palette.bg_dim },
+    a = { fg = palette.bg_dim, bg = palette.red, gui = 'bold' },
+    b = { fg = palette.fg_dim, bg = palette.bg_dim },
+    c = { fg = palette.fg_dim, bg = palette.bg_dim },
   },
 
   replace = {
-    a = { fg = palette.bg_dim, bg = palette.orange },
-    b = { fg = palette.fg_dim,    bg = palette.bg_dim },
-    c = { fg = palette.fg_dim,    bg = palette.bg_dim },
+    a = { fg = palette.bg_dim, bg = palette.orange, gui = 'bold' },
+    b = { fg = palette.fg_dim, bg = palette.bg_dim },
+    c = { fg = palette.fg_dim, bg = palette.bg_dim },
   },
 
   command = {
-    a = { fg = palette.bg_dim, bg = palette.blue },
-    b = { fg = palette.fg_dim,    bg = palette.bg_dim },
-    c = { fg = palette.fg_dim,    bg = palette.bg_dim },
+    a = { fg = palette.bg_dim, bg = palette.blue, gui = 'bold' },
+    b = { fg = palette.fg_dim, bg = palette.bg_dim },
+    c = { fg = palette.fg_dim, bg = palette.bg_dim },
   },
 
   terminal = {
-    a = { fg = palette.bg_dim, bg = palette.magenta },
-    b = { fg = palette.fg_dim,    bg = palette.bg_dim },
-    c = { fg = palette.fg_dim,    bg = palette.bg_dim },
+    a = { fg = palette.bg_dim, bg = palette.magenta, gui = 'bold' },
+    b = { fg = palette.fg_dim, bg = palette.bg_dim },
+    c = { fg = palette.fg_dim, bg = palette.bg_dim },
   },
 
   inactive = {
-    a = { fg = palette.bg_dim, bg = palette.bg0 },
-    b = { fg = palette.fg_dim,    bg = palette.bg_dim },
-    c = { fg = palette.fg_dim,    bg = palette.bg_dim },
+    a = { fg = palette.bg_dim, bg = palette.bg0, gui = 'bold' },
+    b = { fg = palette.fg_dim, bg = palette.bg_dim },
+    c = { fg = palette.fg_dim, bg = palette.bg_dim },
   },
 }
 return theme
