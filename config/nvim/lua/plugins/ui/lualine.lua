d73e0b0a2eca37ad82fe5bef213e40ba005fce1a
Jacob Laws <git@jacoblaws.dev>
2023-09-17 19:06:37 -0700

    neovim: add custom lualine sections

--- config/nvim/lua/plugins/ui/lualine.lua
+++ config/nvim/lua/plugins/ui/lualine.lua
@@ -2,6 +2,9 @@
 --
 -- a blazing fast neovim statusline plugin
 
+local icon = require('core.utils.icons')
+local statusline = require('core.utils.statusline')
+
 local opts = {
   options = {
     theme = 'auto',
@@ -9,9 +12,81 @@ local opts = {
     globalstatus = true,
     icons_enabled = true,
 
+    section_separators = { left = '', right = '', },
     component_separators = { left = '', right = '' },
-    section_separators = { left = '', right = '' },
   },
+
+  sections = {
+    lualine_a = {
+      {
+        'mode',
+        icon = { icon.vim },
+        separator = {
+          left = icon.left_separator,
+          right = icon.right_separator
+        },
+      },
+    },
+    lualine_b = {
+      {
+        'filetype',
+        colored = false,
+        icon_only = true,
+        padding = { left = 2, right = 0 },
+      },
+      {
+        'filename',
+        file_status = false,
+        padding = { left = 1, right = 0 },
+      },
+    },
+    lualine_c = {
+      {
+        'b:gitsigns_head',
+        icon = { icon.git_branch },
+        padding = { left = 2, right = 0 },
+      },
+      {
+        'diff',
+        source = statusline.diff_source,
+        padding = { left = 2, right = 0 },
+      },
+    },
+    lualine_x = {
+      {
+        'diagnostics',
+        sources = { 'nvim_diagnostic' },
+        padding = { left = 0, right = 2 },
+      },
+      {
+        statusline.lsp_name,
+        icon = { icon.lsp },
+        padding = { left = 0, right = 2 },
+      },
+
+    },
+    lualine_y = {},
+    lualine_z = {
+      {
+        statusline.get_cwd,
+        icon = { icon.folder_open, align = 'left' },
+        padding = { left = 1, right = 2 },
+        separator = {
+          left = icon.left_separator,
+          right = '',
+        },
+      },
+      {
+        'progress',
+        icon = { icon.progress, align = 'left' },
+        padding = { left = 0, right = 1 },
+        separator = {
+          left = '',
+          right = icon.right_separator,
+        },
+      },
+    },
+  }
 }
 
 local config = function(_, opts)
