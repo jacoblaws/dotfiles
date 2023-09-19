9830e5910f1f1404a3c71d4a6d1dc39aa9314672
Jacob Laws <git@jacoblaws.dev>
2023-09-19 13:05:27 -0700

    neovim: add super-tab completion to nvim-cmp

--- config/nvim/lua/plugins/lang/lsp/nvim-cmp.lua
+++ config/nvim/lua/plugins/lang/lsp/nvim-cmp.lua
@@ -8,6 +8,12 @@ local opts = function()
   local luasnip = require('luasnip')
   local lspkind = require('lspkind')
 
+  local function has_words_before()
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+  end
+
   return {
     sources = {
       { name = 'nvim_lsp' },
@@ -32,6 +38,32 @@ local opts = function()
         maxwidth = 50,
         ellipsis_char = '...',
       }),
+    },
+
+    mapping = {
+      ['<CR>'] = cmp.mapping.confirm { select = false },
+      ['<Tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        elseif has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+      end,
+      { 'i', 's' }),
+      ['<S-Tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end,
+      { 'i', 's' }),
     },
   }
 end
