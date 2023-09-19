a6afbae8f5589390b206d1e9f1ce4b9a3d1c6240
Jacob Laws <git@jacoblaws.dev>
2023-09-19 15:05:28 -0700

    neovim: add misc nvim-cmp keymaps

--- config/nvim/lua/plugins/lang/lsp/nvim-cmp.lua
+++ config/nvim/lua/plugins/lang/lsp/nvim-cmp.lua
@@ -44,6 +44,9 @@ local opts = function()
       ['<C-u>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
       ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(4),  { 'i', 'c' }),
 
+      ['<C-e>'] = cmp.mapping { i = cmp.mapping.abort(), c = cmp.mapping.close() },
+      ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+
       ['<C-k>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
       ['<C-j>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
       ['<C-p>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
