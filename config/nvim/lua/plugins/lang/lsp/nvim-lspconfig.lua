9f14fea63fc4cbc8d5d26a02cd981ec57dce73a6
Jacob Laws <git@jacoblaws.dev>
2023-10-07 17:43:41 -0700

    neovim: add bashls to lspconfig

--- config/nvim/lua/plugins/lang/lsp/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/lang/lsp/nvim-lspconfig.lua
@@ -8,7 +8,7 @@ local config = function(_, opts)
   local lspconfig = require('lspconfig')
   local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-  local servers = { 'lua_ls' }
+  local servers = { 'lua_ls', 'bashls', }
   for _, lsp in ipairs(servers) do
     lspconfig[lsp].setup { capabilities = capabilities }
   end
