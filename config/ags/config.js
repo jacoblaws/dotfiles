51dcb63083881dbe3f91d1d81ec211ca30b3074a
Jacob Laws <git@jacoblaws.dev>
2023-10-11 15:14:28 -0700

    ags: initial config.js

---
+++ config/ags/config.js
@@ -0,0 +1,12 @@
+export default {
+  maxStreamVolume: 1.05,
+  cacheNotificationActions: false,
+  notificationPopupTimeout: 5000, // milliseconds
+
+  closeWindowDelay: {
+    'window-name': 500, // milliseconds
+  },
+
+  windows: [
+  ],
+};
