5ddb5373eaefa125bd1528001e5f73a6800b4e2c
Jacob Laws <git@jacoblaws.dev>
2023-10-21 23:26:06 -0700

    fish: add $HOME/.local/bin to $PATH

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -21,3 +21,6 @@ alias ls='eza -F   --icons --color=always --sort=ext --group-directories-first'
 alias la='eza -aF  --icons --color=always --sort=ext --group-directories-first'
 alias ll='eza -laF --icons --color=always --sort=ext --group-directories-first'
 alias lt='eza -TF  --icons --color=always --sort=ext --group-directories-first'
+
+# path
+fish_add_path $HOME/.local/bin
