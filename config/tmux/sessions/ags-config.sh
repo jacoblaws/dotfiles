8d7bb59257396937c00c04e4714b4f4fd8f2667c
Jacob Laws <git@jacoblaws.dev>
2023-10-23 10:31:32 -0700

    tmux: tweak ags session

--- config/tmux/sessions/ags-config.sh
+++ config/tmux/sessions/ags-config.sh
@@ -4,13 +4,17 @@
 
 main() {
   local session='ags-config'
-  local path="${HOME}/.config/ags"
+  local path="${HOME}/dotfiles/config/ags"
 
   if ! tmux has-session -t "${selected_session}" >/dev/null 2>&1; then
     tmux new-session -d -s "${session}"  -c "${path}" -n 'nvim'
     tmux new-window  -d -t "${session}": -c "${path}" -n 'ags'
     tmux new-window  -d -t "${session}": -c "${path}" -n 'git'
     tmux select-window -t 1
+
+    tmux send-keys -t "${session}":1 nvim    Enter
+    tmux send-keys -t "${session}":2 ags     Enter
+    tmux send-keys -t "${session}":3 lazygit Enter
   fi
 
   if [[ ! -z "${TMUX}" ]]; then
