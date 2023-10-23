bd5263de85958adc0ee32003e5973a20d9fb939f
Jacob Laws <git@jacoblaws.dev>
2023-10-23 15:11:02 -0700

    ags: add imports.js
    allows for easier importing of ags modules

---
+++ config/ags/js/imports.js
@@ -0,0 +1,13 @@
+import App from 'resource:///com/github/Aylur/ags/app.js';
+import Widget from 'resource:///com/github/Aylur/ags/widget.js';
+import Service from 'resource:///com/github/Aylur/ags/service.js';
+import Variable from 'resource:///com/github/Aylur/ags/variable.js';
+import * as Utils from 'resource:///com/github/Aylur/ags/utils.js';
+
+export {
+  App,
+  Widget,
+  Service,
+  Variable,
+  Utils,
+};
