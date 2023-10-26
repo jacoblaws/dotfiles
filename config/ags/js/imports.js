8b8a567ca92d5d76f9f0479de8ac1a289735f6e9
Jacob Laws <git@jacoblaws.dev>
2023-10-25 23:47:18 -0700

    ags: add GLib to imports.js

--- config/ags/js/imports.js
+++ config/ags/js/imports.js
@@ -4,10 +4,13 @@ import Service from 'resource:///com/github/Aylur/ags/service.js';
 import Variable from 'resource:///com/github/Aylur/ags/variable.js';
 import * as Utils from 'resource:///com/github/Aylur/ags/utils.js';
 
+import GLib from 'gi://GLib';
+
 export {
   App,
   Widget,
   Service,
   Variable,
   Utils,
+  GLib,
 };
