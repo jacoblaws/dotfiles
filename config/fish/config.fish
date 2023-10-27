62cee278763ed7c6e883f0b8509c42a8d0d05dee
Jacob Laws <git@jacoblaws.dev>
2023-10-26 23:39:11 -0700

    fish: add tmux-sessions alias

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -17,6 +17,7 @@ set -g tide_time_format ' %H:%M:%S'
 
 # aliases
 alias lg='lazygit'
+alias tms='tmux-sessions'
 alias ls='eza -F   --icons --color=always --sort=ext --group-directories-first'
 alias la='eza -aF  --icons --color=always --sort=ext --group-directories-first'
 alias ll='eza -laF --icons --color=always --sort=ext --group-directories-first'
