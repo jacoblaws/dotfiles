ffd950757c7dfbcb367725ce1697bf3672c81b9c
Jacob Laws <git@jacoblaws.dev>
2024-01-10 23:54:18 -0800

    flake: add initial flake.nix and flake.lock

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "nixos config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }@inputs: {
+    nixosConfigurations = {
+      "nixos" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./config/nixos/configuration.nix ];
+      };
+    };
+  };
+}
