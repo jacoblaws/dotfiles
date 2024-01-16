bc8e517e18177addc798d753a40f933e94ed0d1b
Jacob Laws <git@jacoblaws.dev>
2024-01-15 22:36:11 -0800

    home-manager: add various gui packages

--- config/home-manager/packages.nix
+++ config/home-manager/packages.nix
@@ -16,5 +16,14 @@
     zip
     unzip
     unrar
+
+    # gui
+    firefox
+    spotify
+    discord
+    obsidian
   ];
+
+  nixpkgs.config.permittedInsecurePackages =
+    pkgs.lib.optional (pkgs.obsidian.version == "1.5.3") "electron-25.9.0";
 }
