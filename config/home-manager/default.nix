8377a5a338c5835c08863ea99c9c93004971b1bc
Jacob Laws <git@jacoblaws.dev>
2024-01-21 16:05:23 -0800

    home-manager: set xdg user directories

--- config/home-manager/default.nix
+++ config/home-manager/default.nix
@@ -1,3 +1,5 @@
+{ config, ... }:
+
 {
   imports = [
     ./neovim.nix
@@ -11,5 +13,17 @@
   xdg.configFile = {
     hypr.source = ../hypr;
     kitty.source = ../kitty;
+  };
+
+  xdg.userDirs = {
+    enable = true;
+    desktop = "${config.home.homeDirectory}/desktop";
+    documents = "${config.home.homeDirectory}/documents";
+    download = "${config.home.homeDirectory}/downloads";
+    music = "${config.home.homeDirectory}/music";
+    pictures = "${config.home.homeDirectory}/pictures";
+    publicShare = "${config.home.homeDirectory}/public";
+    templates = "${config.home.homeDirectory}/templates";
+    videos = "${config.home.homeDirectory}/videos";
   };
 }
