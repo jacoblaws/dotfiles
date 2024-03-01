306b3817d436761a0f2c09e9192638d7dc93e125
Jacob Laws <git@jacoblaws.dev>
2024-02-29 23:13:44 -0800

    flake: add nix-vscode-extensions input

--- flake.nix
+++ flake.nix
@@ -15,6 +15,7 @@
     };
 
     nix-gaming.url = "github:fufexan/nix-gaming";
+    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   };
 
