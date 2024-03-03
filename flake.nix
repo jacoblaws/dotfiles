3bdf94c2764b13bc353ba6dd296bbe13a311ead3
Jacob Laws <git@jacoblaws.dev>
2024-03-03 14:23:16 -0800

    flake: add hyprland input

--- flake.nix
+++ flake.nix
@@ -14,6 +14,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    hyprland.url = "github:hyprwm/hyprland";
     nix-gaming.url = "github:fufexan/nix-gaming";
     nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
