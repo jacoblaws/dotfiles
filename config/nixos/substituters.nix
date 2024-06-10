15f78010e2a1bd369d788ae208e4319810395442
Jacob Laws <git@jacoblaws.dev>
2024-06-10 12:50:17 -0700

    nixos: update substituters

--- config/nixos/substituters.nix
+++ config/nixos/substituters.nix
@@ -1,13 +1,17 @@
 {
   nix.settings = {
     substituters = [
+      "https://cache.nixos.org?priority=10"
       "https://hyprland.cachix.org"
       "https://nix-gaming.cachix.org"
+      "https://nix-community.cachix.org"
     ];
 
     trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 }
