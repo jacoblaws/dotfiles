9ab2da0abe295874a7068f9ace03df8ca1f382e4
Jacob Laws <git@jacoblaws.dev>
2024-08-23 12:09:43 -0700

    hosts: update laptop default config

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -1,4 +1,18 @@
 { inputs, ... }:
 
 {
+  imports = [
+    ./hardware-configuration.nix
+    ../../config/nixos/audio.nix
+    ../../config/nixos/nvidia.nix
+    ../../config/nixos/audio.nix
+    ../../config/nixos/fonts.nix
+    ../../config/nixos/gnome.nix
+    ../../config/nixos/hyprland.nix
+  ];
+
+  networking = {
+    hostName = "nixos-laptop";
+    networkmanager.enable = true;
+  };
 }
