52b14b6915af9e80f4d8e318380f43a7aac83bd2
Jacob Laws <git@jacoblaws.dev>
2024-10-13 20:30:02 -0700

    nixos: update hyprland config

--- config/nixos/hyprland.nix
+++ config/nixos/hyprland.nix
@@ -10,10 +10,6 @@
     xwayland.enable = true;
   };
 
-  xdg.portal = {
-    enable = true;
-  };
-
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1";
     WLR_NO_HARDWARE_CURSORS = "1";
