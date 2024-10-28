66ba615482ba1ff52ac3e52c3aae8b5e61a13f2f
Jacob Laws <git@jacoblaws.dev>
2024-10-27 22:23:58 -0700

    neovim: update plugins init.lua

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -1,20 +1,17 @@
 local plugins = {
-  require('plugins.ui.nvim-web-devicons'),
+  -- utils
+  require('plugins.utils.lazydev'),
+  require('plugins.utils.luasnip'),
+  require('plugins.utils.nvim-web-devicons'),
 
   -- colorschemes
   require('plugins.themes.everforest'),
 
-  -- lang utils
-  require('plugins.lang.utils.mason'),
-  require('plugins.lang.utils.luasnip'),
-  require('plugins.lang.utils.neodev'),
-
   -- lsp
-  require('plugins.lang.lsp.lspkind'),
-  require('plugins.lang.lsp.nvim-cmp'),
-  require('plugins.lang.lsp.cmp-nvim-lsp'),
-  require('plugins.lang.lsp.mason-lspconfig'),
-  require('plugins.lang.lsp.nvim-lspconfig'),
+  require('plugins.lsp.mason'),
+  require('plugins.lsp.mason-lspconfig'),
+  require('plugins.lsp.nvim-lspconfig'),
+  require('plugins.lsp.nvim-cmp'),
 
   -- language tools
   require('plugins.lang.markdown-preview'),
