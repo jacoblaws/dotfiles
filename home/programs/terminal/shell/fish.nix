94d662bf6bce2aeb511b2f10feff712eb902e6da
Jacob Laws <git@jacoblaws.dev>
2024-12-06 23:18:16 -0800

    fish: add git helper functions

--- home/programs/terminal/shell/fish.nix
+++ home/programs/terminal/shell/fish.nix
@@ -18,16 +18,26 @@
     '';
 
     shellAliases = {
-      lg = "lazygit";
       tms = "tmux-sessions";
       ls = "eza -F   --icons --color=always --sort=ext --group-directories-first";
       la = "eza -aF  --icons --color=always --sort=ext --group-directories-first";
       ll = "eza -laF --icons --color=always --sort=ext --group-directories-first";
       lt = "eza -TF  --icons --color=always --sort=ext --group-directories-first";
 
+      # git
+      lg = "lazygit";
+      gl = "git log --pretty=fuller";
+      gs = "git stash";
+      gsp = "git stash pop";
+
       # emacs distributions
       dm = "emacs --init-dir ~/.local/share/doom-emacs";
       sm = "emacs --init-dir ~/.local/share/spacemacs";
+    };
+
+    functions = {
+      gsr = "git reset --soft HEAD~$argv[1]";
+      gcad = "git rebase --committer-date-is-author-date HEAD~$argv[1]";
     };
   };
 }
