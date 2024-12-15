86279b8d42793257c538a0593982c5d818b26e2f
Jacob Laws <git@jacoblaws.dev>
2024-12-14 16:00:08 -0800

    hyprland: update desktop monitor settings

--- home/profiles/desktop/default.nix
+++ home/profiles/desktop/default.nix
@@ -11,8 +11,8 @@
 
   wayland.windowManager.hyprland.settings = {
     monitor = [
-      "DP-1, 3440x1440@144, 0x0, 1, bitdepth, 10, vrr, 1"
-      "DP-2, 2560x1440@144, 3440x0, 1, transform, 1, bitdepth, 10, vrr, 1"
+      "DP-1, 3440x1440@144, 0x0, 1, bitdepth, 10, vrr, 0"
+      "DP-2, 2560x1440@144, 3440x0, 1, transform, 1, bitdepth, 10, vrr, 0"
     ];
   };
 }
