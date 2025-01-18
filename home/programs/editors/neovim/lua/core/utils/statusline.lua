4eb7a2bb2ae4ba9f5bb0b066053b097255f7bb81
Jacob Laws <git@jacoblaws.dev>
2025-01-17 18:58:16 -0800

    neovim: update statusline utils
    vim.api.nvim_buf_get_option() and vim.lsp.get_active_clients() were
    deprecated

--- home/programs/editors/neovim/lua/core/utils/statusline.lua
+++ home/programs/editors/neovim/lua/core/utils/statusline.lua
@@ -4,18 +4,17 @@ function M.get_cwd()
   return vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
 end
 
-function M.get_current_filetype()
-  return vim.api.nvim_buf_get_option(0, 'filetype')
-end
-
 function M.lsp_name()
-  local clients = vim.lsp.get_active_clients()
-  if next(clients) == nil then return '' end
+  local clients = vim.lsp.get_clients({ bufnr = 0 })
+  if #clients == 0 then
+    return ''
+  end
 
-  local buf_ft = M.get_current_filetype()
+  local buf_ft = vim.bo.filetype
   for _, client in ipairs(clients) do
+    ---@diagnostic disable-next-line: undefined-field
     local filetypes = client.config.filetypes
-    if filetypes and vim.fn.index(filetypes, buf_ft) ~= 1 then
+    if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
       return client.name
     end
   end
