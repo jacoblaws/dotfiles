2b8435c3f9f7df28d8203da389242a82c9ee7cfa
Jacob Laws <git@jacoblaws.dev>
2025-03-11 13:34:12 -0700

    home: add orca-slicer package

--- home/packages.nix
+++ home/packages.nix
@@ -54,6 +54,18 @@
     # misc
     nwg-look
     gnome-tweaks
+
+    (pkgs.symlinkJoin {
+      name = "orca-slicer";
+      paths = [ pkgs.orca-slicer ];
+      buildInputs = [ pkgs.makeWrapper ];
+      postBuild = ''
+          wrapProgram $out/bin/orca-slicer \
+          --set __GLX_VENDOR_LIBRARY_NAME mesa \
+          --set __EGL_VENDOR_LIBRARY_FILENAMES ${pkgs.mesa.drivers}/share/glvnd/egl_vendor.d/50_mesa.json
+      '';
+      meta.mainProgram = "orca-slicer";
+    })
   ];
 
   nixpkgs.config.permittedInsecurePackages =
