58c5a50b3fd42e915d20d7f67fdc2195959b8c3b
Jacob Laws <git@jacoblaws.dev>
2025-03-30 15:47:24 -0700

    nixos: fix nh flake path

--- system/nix/nh.nix
+++ system/nix/nh.nix
@@ -1,8 +1,8 @@
-{ self, ... }: {
+{
   programs.nh = {
     enable = true;
     clean.enable = true;
     clean.extraArgs = "--keep 5 --keep-since 1w";
-    flake = "${self}";
+    flake = "/home/jvl/dotfiles";
   };
 }
