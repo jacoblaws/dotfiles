b7cd12ada591e0958a0f3752d37d8ed52beef47d
Jacob Laws <git@jacoblaws.dev>
2025-04-03 23:44:04 -0700

    nixos: add niri cachix substituters

--- system/nix/substituters.nix
+++ system/nix/substituters.nix
@@ -2,6 +2,7 @@
   nix.settings = {
     substituters = [
       "https://cache.nixos.org?priority=10"
+      "https://niri.cachix.org"
       "https://hyprland.cachix.org"
       "https://nix-gaming.cachix.org"
       "https://nix-community.cachix.org"
@@ -9,6 +10,7 @@
 
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
