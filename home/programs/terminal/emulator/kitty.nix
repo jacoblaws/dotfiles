5621464e40e82eaf8b12d7f74033920cf5a0923a
Jacob Laws <git@jacoblaws.dev>
2025-04-26 15:30:43 -0700

    home: update kitty config

--- home/programs/terminal/emulator/kitty.nix
+++ home/programs/terminal/emulator/kitty.nix
@@ -1,4 +1,7 @@
-{ ... }: {
+{ osConfig, ... }: let
+  theme = osConfig.theme;
+  palette = theme.palette.${theme.name}.colors;
+in {
   programs.kitty = {
     enable = true;
 
@@ -21,7 +24,72 @@
       window_padding_width = "0";
       clear_all_shortcuts = "yes";
     };
+  };
 
-    themeFile = "everforest_dark_medium";
+  # color scheme
+  programs.kitty.settings = {
+    foreground = "#${palette.fg}";
+    background = "#${palette.bg0}";
+    selection_foreground = "#${palette.grey2}";
+    selection_background = "#${palette.bg4}";
+
+    cursor = "#${palette.fg}";
+    cursor_text_color = "#${palette.bg1}";
+
+    url_color = "#${palette.blue}";
+
+    active_border_color = "#${palette.green}";
+    inactive_border_color = "#${palette.bg5}";
+    bell_border_color = "#${palette.orange}";
+    visual_border_color = "none";
+
+    wayland_titlebar_color = "system";
+    macos_titlebar_color = "system";
+
+    active_tab_foreground = "#${palette.fg}";
+    active_tab_background = "#${palette.bgd}";
+    inactive_tab_foreground = "#${palette.grey2}";
+    inactive_tab_background = "#${palette.bg2}";
+    tab_bar_background = "#${palette.bg1}";
+    tab_bar_margin_color = "none";
+
+    mark1_foreground = "#${palette.bg0}";
+    mark1_background = "#${palette.blue}";
+    mark2_foreground = "#${palette.bg0}";
+    mark2_background = "#${palette.yellow}";
+    mark3_foreground = "#${palette.bg0}";
+    mark3_background = "#${palette.magenta}";
+
+    # black
+    color0 = "#${palette.bg1}";
+    color8 = "#${palette.bg1}";
+
+    # red
+    color1 = "#${palette.red}";
+    color9 = "#${palette.red}";
+
+    # green
+    color2  = "#${palette.green}";
+    color10 = "#${palette.green}";
+
+    # yellow
+    color3  = "#${palette.yellow}";
+    color11 = "#${palette.yellow}";
+
+    # blue
+    color4  = "#${palette.blue}";
+    color12 = "#${palette.blue}";
+
+    # magenta
+    color5  = "#${palette.magenta}";
+    color13 = "#${palette.magenta}";
+
+    # cyan
+    color6  = "#${palette.cyan}";
+    color14 = "#${palette.cyan}";
+
+    # white
+    color7  = "#${palette.fg}";
+    color15 = "#${palette.fg}";
   };
 }
