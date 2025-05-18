600c3a9d3a899d7667ff1ab8051d0c6bc1ab17b0
Jacob Laws <git@jacoblaws.dev>
2025-05-17 22:07:52 -0700

    home: move xdg specifications to a separate file

--- home/default.nix
+++ home/default.nix
@@ -19,42 +19,4 @@
       nix-direnv.enable = true;
     };
   };
-
-  xdg.userDirs = {
-    enable = true;
-    desktop = "${config.home.homeDirectory}/desktop";
-    documents = "${config.home.homeDirectory}/docs";
-    download = "${config.home.homeDirectory}/downloads";
-    music = "${config.home.homeDirectory}/media/audio/music";
-    pictures = "${config.home.homeDirectory}/media/images";
-    publicShare = "${config.home.homeDirectory}/public";
-    templates = "${config.home.homeDirectory}/templates";
-    videos = "${config.home.homeDirectory}/media/videos";
-  };
-
-  xdg.mimeApps = {
-    enable = true;
-
-    associations.added = {
-      "application/pdf" = [ "zathura.desktop" "sioyek.desktop" ];
-      "default-web-browser" = [ "firefox.desktop" ];
-      "text/html" = [ "firefox.desktop" ];
-      "x-scheme-handler/http" = [ "firefox.desktop" ];
-      "x-scheme-handler/https" = [ "firefox.desktop" ];
-      "x-scheme-handler/about" = [ "firefox.desktop" ];
-      "x-scheme-handler/unkown" = [ "firefox.desktop" ];
-      "inode/directory" = [ "nemo.desktop" ];
-    };
-
-    defaultApplications = {
-      "application/pdf" = [ "zathura.desktop" "sioyek.desktop" ];
-      "default-web-browser" = [ "firefox.desktop" ];
-      "text/html" = [ "firefox.desktop" ];
-      "x-scheme-handler/http" = [ "firefox.desktop" ];
-      "x-scheme-handler/https" = [ "firefox.desktop" ];
-      "x-scheme-handler/about" = [ "firefox.desktop" ];
-      "x-scheme-handler/unkown" = [ "firefox.desktop" ];
-      "inode/directory" = [ "nemo.desktop" ];
-    };
-  };
 }
---
+++ home/programs/terminal/tools/xdg.nix
@@ -0,0 +1,39 @@
+{ config, ... }: let
+  homeDir = config.home.homeDirectory;
+in {
+  xdg.userDirs = {
+    enable = true;
+    desktop = "${homeDir}/desktop";
+    documents = "${homeDir}/docs";
+    download = "${homeDir}/downloads";
+    music = "${homeDir}/media/audio/music";
+    pictures = "${homeDir}/media/images";
+    publicShare = "${homeDir}/public";
+    templates = "${homeDir}/templates";
+    videos = "${homeDir}/media/videos";
+  };
+
+  xdg.mimeApps = {
+    enable = true;
+    associations.added = {
+      "application/pdf" = [ "zathura.desktop" "sioyek.desktop" ];
+      "default-web-browser" = [ "firefox.desktop" ];
+      "inode/directory" = [ "nemo.desktop" ];
+      "text/html" = [ "firefox.desktop" ];
+      "x-scheme-handler/about" = [ "firefox.desktop" ];
+      "x-scheme-handler/http" = [ "firefox.desktop" ];
+      "x-scheme-handler/https" = [ "firefox.desktop" ];
+      "x-scheme-handler/unkown" = [ "firefox.desktop" ];
+    };
+    defaultApplications = {
+      "application/pdf" = [ "zathura.desktop" "sioyek.desktop" ];
+      "default-web-browser" = [ "firefox.desktop" ];
+      "inode/directory" = [ "nemo.desktop" ];
+      "text/html" = [ "firefox.desktop" ];
+      "x-scheme-handler/about" = [ "firefox.desktop" ];
+      "x-scheme-handler/http" = [ "firefox.desktop" ];
+      "x-scheme-handler/https" = [ "firefox.desktop" ];
+      "x-scheme-handler/unkown" = [ "firefox.desktop" ];
+    };
+  };
+}
