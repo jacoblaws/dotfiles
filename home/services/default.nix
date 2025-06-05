3d7fdb0701c6e4bee06b6ef4e3eb9b76c6d78fe7
Jacob Laws <git@jacoblaws.dev>
2025-06-04 21:51:41 -0700

    home: enable darkman service

--- home/services/default.nix
+++ home/services/default.nix
@@ -1,4 +1,13 @@
 {
+  services.darkman = {
+    enable = true;
+    settings = {
+      usegeoclue = false;
+      dbusserver = true;
+      portal = true;
+    };
+  };
+
   services.hyprpaper = {
     enable = true;
     settings = {
