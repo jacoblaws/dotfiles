fef79550e1398ffc73a1c2a4f858c0de67a10253
Jacob Laws <git@jacoblaws.dev>
2025-06-12 23:35:42 -0700

    home: add various media packages

--- home/programs/media/default.nix
+++ home/programs/media/default.nix
@@ -1,5 +1,14 @@
-{
+{ pkgs, ... }: {
   imports = [
     ./tauon
   ];
+
+  home.packages = with pkgs; [
+    ffmpeg
+    fluidsynth
+    mpv
+    soundfont-fluid
+  ];
+
+  programs.timidity.enable = true;
 }
