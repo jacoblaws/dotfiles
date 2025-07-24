63f4bfa7f72323e54c590be000fd45b25b665da7
Jacob Laws <git@jacoblaws.dev>
2025-07-23 19:22:55 -0700

    neovim: update conform config

--- home/programs/editors/neovim/lua/plugins/lsp/conform.lua
+++ home/programs/editors/neovim/lua/plugins/lsp/conform.lua
@@ -1,52 +1,63 @@
 local plugin = {
-	"stevearc/conform.nvim",
-	lazy = true,
-	cmd = "ConformInfo",
-	event = "BufWritePre",
+  'stevearc/conform.nvim',
+  lazy = true,
+  cmd = 'ConformInfo',
+  event = 'BufWritePre',
 }
 
 plugin.keys = {
-	{
-		"<leader>cf",
-		function()
-			require("conform").format()
-		end,
-		mode = { "n", "v" },
-		desc = "Format",
-	},
-	{
-		"<leader>cF",
-		function()
-			require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
-		end,
-		mode = { "n", "v" },
-		desc = "Format injected langs",
-	},
+  {
+    '<leader>cf',
+    function()
+      require('conform').format()
+    end,
+    mode = { 'n', 'v' },
+    desc = 'Format',
+  },
+  {
+    '<leader>cF',
+    function()
+      require('conform').format({
+        formatters = { 'injected' },
+        timeout_ms = 3000,
+      })
+    end,
+    mode = { 'n', 'v' },
+    desc = 'Format injected langs',
+  },
 }
 
 plugin.opts = {
-	default_format_opts = {
-		timeout_ms = 3000,
-		async = false,
-		quiet = false,
-		lsp_format = "fallback",
-	},
+  formatters_by_ft = {
+    lua = { 'stylua' },
+    nix = { 'nixfmt' },
+    python = { 'ruff_format' },
+    rust = { 'rustfmt' },
+    toml = { 'taplo' },
+    haskell = { 'ormolu' },
+  },
 
-	formatters_by_ft = {
-		nix = { "nixfmt" },
-		lua = { "stylua" },
-		rust = { "rustfmt" },
-		toml = { "taplo" },
-		python = { "ruff_format" },
-	},
+  default_format_opts = {
+    async = false,
+    quiet = false,
+    timeout_ms = 3000,
+    lsp_format = 'fallback',
+  },
 
-	formatters = {
-		injected = { options = { ignore_errors = true } },
-	},
+  formatters = {
+    injected = { options = { ignore_errors = true } },
+  },
 }
 
 plugin.config = function(_, opts)
-	require("conform").setup(opts)
+  require('conform').setup(opts)
+
+  vim.api.nvim_create_autocmd('BufWritePre', {
+    pattern = '*',
+    callback = function(args)
+      require('conform').format({ bufnr = args.buf })
+    end,
+  })
 end
 
 return plugin
