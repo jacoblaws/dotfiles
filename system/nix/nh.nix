3a34c58c60ae851489239c894e188b8f8c06beb3
Jacob Laws <git@jacoblaws.dev>
2025-07-31 13:18:31 -0700

    nixos: disable scheduled cleaning of gcroots with nh

--- system/nix/nh.nix
+++ system/nix/nh.nix
@@ -2,7 +2,7 @@
   programs.nh = {
     enable = true;
     clean.enable = true;
-    clean.extraArgs = "--keep 5 --keep-since 1w";
+    clean.extraArgs = "--keep 5 --keep-since 1w --nogcroot";
     flake = "/home/jvl/dotfiles";
   };
 }
