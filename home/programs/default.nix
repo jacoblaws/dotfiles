2529384baa6f7b6c30c6ea4bc329a0ea8cba8fad
Jacob Laws <git@jacoblaws.dev>
2025-08-15 19:16:28 -0700

    home: swap to standard orca-slicer package

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -16,20 +16,8 @@
     libreoffice
     nemo
     obsidian
+    orca-slicer
     protonvpn-gui
     zotero
-
-    (pkgs.symlinkJoin {
-      name = "orca-slicer";
-      paths = [ pkgs.orca-slicer ];
-      buildInputs = [ pkgs.makeWrapper ];
-      postBuild = ''
-          wrapProgram $out/bin/orca-slicer \
-          --set __GLX_VENDOR_LIBRARY_NAME mesa \
-          --set __EGL_VENDOR_LIBRARY_FILENAMES ${pkgs.mesa.drivers}/share/glvnd/egl_vendor.d/50_mesa.json
-      '';
-      meta.mainProgram = "orca-slicer";
-    })
-
   ];
 }
