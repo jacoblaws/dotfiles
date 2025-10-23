c12b2eec0fec9a07def7762f5d43f88287f55847
Jacob Laws <git@jacoblaws.dev>
2025-10-23 10:22:48 -0700

    flake: restructure flake

--- flake.nix
+++ flake.nix
@@ -1,40 +1,53 @@
 {
   description = "NixOS and Home-Manager flake";
 
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ ./hosts ./modules ];
+      systems = [ "x86_64-linux" ];
+      perSystem = { pkgs, ... }: {
+        devShells.default = pkgs.mkShell {
+          name = "dotfiles";
+          packages = with pkgs; [
+            lua-language-server
+            nixd
+            nixfmt-classic
+            statix
+            stylua
+          ];
+        };
+      };
+    };
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:/hercules-ci/flake-parts";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
 
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    zjstatus.url = "github:dj95/zjstatus";
-    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
-    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
-  };
-
-  outputs = inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [
-        ./hosts
-        ./modules
-      ];
-
-      systems = [ "x86_64-linux" ];
-
-      perSystem = { pkgs, ... }: {
-        devShells.default = pkgs.mkShell {
-          name = "dotfiles";
-          packages = with pkgs; [
-            nixd
-            statix
-            nixfmt-classic
-            lua-language-server
-            stylua
-          ];
-        };
+    neovim-nightly-overlay = {
+      url = "github:nix-community/neovim-nightly-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-parts.follows = "flake-parts";
       };
     };
+
+    nix-vscode-extensions = {
+      url = "github:nix-community/nix-vscode-extensions";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    zjstatus = {
+      url = "github:dj95/zjstatus";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 }
