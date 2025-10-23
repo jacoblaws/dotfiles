8cb2d502db0aa774f0541bbe29b5a010307056d6
Jacob Laws <git@jacoblaws.dev>
2025-10-23 10:40:29 -0700

    nixos: update font config

--- system/core/fonts.nix
+++ system/core/fonts.nix
@@ -34,13 +34,13 @@
       hinting = {
         enable = true;
         autohint = false;
-        style = "slight";
+        style = "full";
       };
 
       defaultFonts = {
         serif = [ "Libertinus Serif" ];
         sansSerif = [ "Recursive Sans Casual Static" ];
-        monospace = [ "Rec Mono Casual" "Symbols Nerd Font" ];
+        monospace = [ "Rec Mono Casual" "Symbols Nerd Font Mono" ];
         emoji = [ "Noto Color Emoji" ];
       };
 
@@ -50,16 +50,16 @@
             <string>Recursive</string>
           </test>
           <edit name="fontfeatures" mode="append">
-            <string>dlig on</string> <!-- code ligatures -->
-            <string>ss01 on</string> <!-- no-serif 'L' and 'Z' -->
-            <string>ss02 on</string> <!-- no-serif 'L' and 'Z' -->
-            <string>ss03 on</string> <!-- no-serif 'L' and 'Z' -->
-            <string>ss04 on</string> <!-- no-serif 'L' and 'Z' -->
-            <string>ss05 on</string> <!-- no-serif 'L' and 'Z' -->
-            <string>ss06 on</string> <!-- no-serif 'L' and 'Z' -->
-            <string>ss07 on</string> <!-- no-serif 'L' and 'Z' -->
-            <string>ss08 on</string> <!-- no-serif 'L' and 'Z' -->
-            <string>ss12 on</string> <!-- simplified mono 'at' -->
+            <string>dlig on</string>  <!-- code ligatures -->
+            <string>ss01 on</string>  <!-- single-story 'a' -->
+            <string>ss02 on</string>  <!-- single-story 'g' -->
+            <string>ss03 off</string> <!-- simplified 'f' -->
+            <string>ss04 on</string>  <!-- simplified 'i' -->
+            <string>ss05 on</string>  <!-- simplified 'l' -->
+            <string>ss06 on</string>  <!-- simplified 'r' -->
+            <string>ss07 on</string>  <!-- simplified italid diagonals -->
+            <string>ss08 on</string>  <!-- no-serif 'L' & 'Z' -->
+            <string>ss12 on</string>  <!-- simplified mono '@' -->
           </edit>
         </match>
       '';
