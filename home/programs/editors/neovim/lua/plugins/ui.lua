76cf51b0db2f423e5d43a6123560d88b89f871bd
Jacob Laws <git@jacoblaws.dev>
2025-10-30 00:34:01 -0700

    nvim: add which-key

--- home/programs/editors/neovim/lua/plugins/ui.lua
+++ home/programs/editors/neovim/lua/plugins/ui.lua
@@ -1,4 +1,23 @@
-vim.pack.add({{ src = 'https://github.com/A7Lavinraj/fyler.nvim', version = 'stable' }})
+vim.pack.add({
+  { src = 'https://github.com/folke/which-key.nvim' },
+  { src = 'https://github.com/A7Lavinraj/fyler.nvim', version = 'stable' },
+})
+
+require('which-key').setup({
+  preset = 'helix',
+  icons = {
+    group = '',
+    mappings = false,
+  },
+})
+
+require('which-key').add({
+  { '<leader>b', group = '󰌨 Buffer' },
+  { '<leader>f', group = '󰍉 Find' },
+  { '<leader>g', group = '󰊢 Git' },
+  { '<leader>l', group = '󰅴 Language' },
+  { '<leader>t', group = '󰔡 Toggle' },
+})
 
 require('fyler').setup({
   confirm_simple = true,
