f2a3f050990d84b2e736a19bcc39a073fe65c70e
Jacob Laws <git@jacoblaws.dev>
2025-11-02 13:11:48 -0800

    nvim: add cokeline

--- home/programs/editors/neovim/lua/plugins/ui.lua
+++ home/programs/editors/neovim/lua/plugins/ui.lua
@@ -6,6 +6,7 @@ vim.pack.add({
   { src = 'https://github.com/folke/which-key.nvim' },
   { src = 'https://github.com/A7Lavinraj/fyler.nvim', version = 'stable' },
   { src = 'https://github.com/nvim-neo-tree/neo-tree.nvim', version = vim.version.range('3') },
+  { src = 'https://github.com/willothy/nvim-cokeline' },
 })
 
 require('which-key').setup({
@@ -86,6 +87,77 @@ require('neo-tree').setup({
       ['l'] = 'open',
       ['h'] = 'close_node',
       ['P'] = 'toggle_preview',
+    },
+  },
+})
+
+require('cokeline').setup({
+  show_if_buffers_are_at_least = 1,
+
+  fill_hl = 'TabLine',
+  default_hl = {
+    fg = function(buffer)
+      return buffer.is_focused and vim.api.nvim_get_hl(0, { name = 'Normal' }).fg
+        or vim.api.nvim_get_hl(0, { name = 'TabLine' }).fg
+    end,
+    bg = function(buffer)
+      return buffer.is_focused and vim.api.nvim_get_hl(0, { name = 'Normal' }).bg
+        or vim.api.nvim_get_hl(0, { name = 'TabLine' }).bg
+    end,
+  },
+
+  sidebar = {
+    filetype = 'neo-tree',
+    components = {
+      {
+        text = '󰐅 File Tree',
+        fg = 'Function',
+        bg = 'TabLine',
+        style = 'bold',
+      },
+    },
+  },
+
+  components = {
+    {
+      text = function(buffer)
+        local function is_neotree_open()
+          if not package.loaded['neo-tree'] then return nil end
+
+          local get_state = require('neo-tree.sources.manager').get_state
+          local renderer = require('neo-tree.ui.renderer')
+
+          local file_open = renderer.window_exists(get_state('filesystem'))
+          local buf_open = renderer.window_exists(get_state('buffers'))
+          local git_open = renderer.window_exists(get_state('git_status'))
+
+          return file_open or buf_open or git_open
+        end
+
+        local neotree_state = is_neotree_open()
+        return (buffer.index == 1 and neotree_state) and '▋' or ''
+      end,
+      fg = 'WinSeparator',
+    },
+    {
+      text = function(buffer) return ' ' .. buffer.devicon.icon end,
+    },
+    {
+      text = function(buffer) return buffer.unique_prefix end,
+    },
+    {
+      text = function(buffer) return buffer.filename .. ' ' end,
+    },
+    {
+      text = function(buffer)
+        if buffer.is_readonly then return '' end
+        if buffer.is_modified then return '' end
+        return ''
+      end,
+      delete_buffer_on_left_click = true,
+    },
+    {
+      text = ' ',
     },
   },
 })
