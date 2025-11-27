e407e99208ba6464a4023b83396f9019506c6c6d
Jacob Laws <git@jacoblaws.dev>
2025-11-27 12:12:11 -0800

    home: add noctalia-shell

--- flake.nix
+++ flake.nix
@@ -45,6 +45,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    noctalia = {
+      url = "github:noctalia-dev/noctalia-shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
