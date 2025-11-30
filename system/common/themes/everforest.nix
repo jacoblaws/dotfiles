792b13f599f01ce4d0da790da0e8852436cef8bf
Jacob Laws <git@jacoblaws.dev>
2025-11-29 17:47:05 -0800

    system: tweak everforest theme

--- system/common/themes/everforest.nix
+++ system/common/themes/everforest.nix
@@ -1,6 +1,18 @@
 {
   theme.everforest = {
     dark = {
+      red = "e67e80";
+      yellow = "dbbc7f";
+      green = "a7c080";
+      blue = "7fbbb3";
+      magenta = "d699b6";
+      cyan = "83c092";
+      orange = "e69875";
+
+      grey0 = "7a8478";
+      grey1 = "859289";
+      grey2 = "9da9a0";
+
       fg = "d3c6aa";
       bgd = "232a2e";
       bg0 = "2d353b";
@@ -9,27 +21,31 @@
       bg3 = "475258";
       bg4 = "4f585e";
       bg5 = "56635f";
+
       bgr = "514045";
       bgy = "4d4c43";
       bgg = "425047";
       bgb = "3a515d";
-      bgp = "4a444e";
+      bgm = "4a444e";
       bgv = "543a48";
-      grey0 = "7a8478";
-      grey1 = "859289";
-      grey2 = "9da9a0";
-      red = "e67e80";
-      yellow = "dbbc7f";
-      green = "a7c080";
-      blue = "7fbbb3";
-      magenta = "d699b6";
-      cyan = "83c092";
-      orange = "e69875";
+
       sl1 = "a7c080";
       sl2 = "d3c6aa";
       sl3 = "e67e80";
     };
     light = {
+      red = "f85552";
+      yellow = "dfa000";
+      green = "8da101";
+      blue = "3a94c5";
+      magenta = "df69ba";
+      cyan = "35a77c";
+      orange = "f57d26";
+
+      grey0 = "a6b0a0";
+      grey1 = "939f91";
+      grey2 = "829181";
+
       fg = "5c6a72";
       bgd = "efebd4";
       bg0 = "fdf6e3";
@@ -38,22 +54,14 @@
       bg3 = "e6e2cc";
       bg4 = "e0dcc7";
       bg5 = "bdc3af";
+
       bgr = "fde3da";
       bgy = "faedcd";
       bgg = "f0f1d2";
       bgb = "e9f0e9";
-      bgp = "fae8e2";
+      bgm = "fae8e2";
       bgv = "eaedc8";
-      grey0 = "a6b0a0";
-      grey1 = "939f91";
-      grey2 = "829181";
-      red = "f85552";
-      yellow = "dfa000";
-      green = "8da101";
-      blue = "3a94c5";
-      magenta = "df69ba";
-      cyan = "35a77c";
-      orange = "f57d26";
+
       sl1 = "93b259";
       sl2 = "708089";
       sl3 = "e66868";
