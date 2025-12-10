41b8d44d8eac2f1fc1bd8c38831e318017ef71cd
Jacob Laws <git@jacoblaws.dev>
2025-12-09 20:46:16 -0800

    nixos: update desktop monitor settings

--- home/profiles/desktop/default.nix
+++ home/profiles/desktop/default.nix
@@ -1,9 +1,9 @@
-{ config, ... }: let
-  homeDir = config.home.homeDirectory;
+{ config, ... }:
+let homeDir = config.home.homeDirectory;
 in {
   wayland.windowManager.hyprland.settings = {
     monitor = [
-      "DP-1, 3440x1440@144, 0x0, 1, bitdepth, 10, vrr, 0"
+      "DP-3, 3440x1440@144, 0x0, 1, bitdepth, 10, vrr, 0"
       "DP-2, 2560x1440@144, 3440x0, 1, transform, 1, bitdepth, 10, vrr, 0"
     ];
   };
@@ -15,7 +15,7 @@ in {
     ];
 
     wallpaper = [
-      "DP-1, ${homeDir}/media/images/wallpapers/uw/current.png"
+      "DP-3, ${homeDir}/media/images/wallpapers/uw/current.png"
       "DP-2, ${homeDir}/media/images/wallpapers/v/current.png"
     ];
   };
