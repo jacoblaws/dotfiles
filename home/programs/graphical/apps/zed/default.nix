23a543e593fabfe1115c3bf7103807be04f4032c
Jacob Laws <git@jacoblaws.dev>
2026-03-13 14:24:22 -0700

    zed: update config

---
+++ home/programs/graphical/apps/zed/default.nix
@@ -0,0 +1,47 @@
+{ config, pkgs, ... }:
+let
+  inherit (config.home) homeDirectory;
+  inherit (config.lib.file) mkOutOfStoreSymlink;
+in {
+  programs.zed-editor = {
+    enable = true;
+    extraPackages = with pkgs; [
+      libgcc
+      tree-sitter
+
+      # Language servers
+      bash-language-server
+      clang-tools
+      haskell-language-server
+      lua-language-server
+      marksman
+      nil
+      nixd
+      nushell
+      package-version-server
+      rust-analyzer
+      taplo
+      texlab
+      tinymist
+      ty
+      typescript-language-server
+      vscode-json-languageserver
+
+      # Formatters
+      fourmolu
+      ruff
+      stylua
+      nixfmt-classic
+      shfmt
+      rustfmt
+      nufmt
+      typstyle
+    ];
+  };
+
+  xdg.configFile."zed/settings.json".source = mkOutOfStoreSymlink
+    "${homeDirectory}/dotfiles/home/programs/graphical/apps/zed/settings.json";
+
+  xdg.configFile."zed/keymap.json".source = mkOutOfStoreSymlink
+    "${homeDirectory}/dotfiles/home/programs/graphical/apps/zed/keymap.json";
+}
